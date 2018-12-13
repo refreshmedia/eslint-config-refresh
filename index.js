219832bcd0724c23a25723a1ce8dafd3bff0c8a1
Tyler Smith <hello@tylerbuilds.co>
2018-12-12 23:56:15 -0800

    hello, world

---
+++ index.js
@@ -0,0 +1,13 @@
+'use strict'
+
+module.exports = {
+  'parserOptions': {
+    'ecmaVersion': '2018'
+  },
+  'extends': ['standard'],
+  'rules': {
+    'jsx-quotes': [2, 'prefer-single'],
+    'no-var': 2,
+    'arrow-parens': 2
+  }
+}
---
+++ react.js
@@ -0,0 +1,6 @@
+'use strict'
+
+module.exports = {
+  'extends': ['standard', 'standard-react'],
+  'parser': 'babel-eslint'
+}
