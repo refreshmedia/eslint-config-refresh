75e89a6994afe24ae09b42bec94e40cef1008e47
Tyler Smith <hello@tylerbuilds.co>
2020-01-14 11:34:19 -0800

    adding custom rules to react

--- react.js
+++ react.js
@@ -2,5 +2,16 @@
 
 module.exports = {
   parser: 'babel-eslint',
-  extends: ['standard', 'standard-react']
+  extends: ['standard', 'standard-react'],
+  'rules': {
+    'no-unused-vars': [
+      'error',
+      {
+        'vars': 'all',
+        'args': 'after-used',
+        'ignoreRestSiblings': false
+      }
+    ],
+    'react/prop-types': 0
+  }
 }
